8a26f974f67dba19d2d4b0eca660a475fd248bc0
Anastazija Verovic <anastazijaverovic@gmail.com>
2020-03-09 15:57:00 +0100

    Backward Elimination - manual and automatic algorithms

--- Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.R
+++ Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.R
@@ -3,7 +3,7 @@
 # Importing the dataset
 dataset = read.csv('50_startups.csv')
 
-# Encoding categorical data
+#Encoding categorical data
 
 dataset$State = factor(dataset$State,
                  levels = c('California','Florida','New York'),
@@ -21,51 +21,17 @@ test_set = subset(dataset, split == FALSE)
 # training_set = scale(training_set)
 # test_set = scale(test_set)
 
-# Fitting Multiple Linear Regression to the Training set with all independent variables
+#Fitting Multiple Linear Regression to the Training set
 
 regressor = lm(formula = Profit ~ .,
                data = training_set)
-
-# Predicting the Test set results
+#Predicting the Test set results
 
 y_pred = predict(regressor, newdata = test_set)
 
-# using only R.D.Spend variable to fit the model because it is the most significant
 
 regressor_rd = lm(formula = Profit ~ R.D.Spend,
                data = training_set)
-
-# Predicting the Test set results
+#Predicting the Test set results
 
 y_pred_rd = predict(regressor_rd, newdata = test_set)
-
-
-# Building the optimal model using Backward Elimination
-
-regressor = lm(formula = Profit ~ R.D.Spend + Administration + Marketing.Spend + State,
-               data = dataset)
-
-summary(regressor)
-
-# remove the variable with the  biggest p-value = State
-# fit the model without removed variable
-
-regressor = lm(formula = Profit ~ R.D.Spend + Administration + Marketing.Spend,
-               data = dataset)
-
-summary(regressor)
-
-#to-do: get r value of each predictor in regressor
-#and name of each predictor from regressor
-
-for(r in regressor$coefficients) 
-{
-  if(summary(regressor)$r.squared  < 0.05){
-    print("Predictor can stay:",regressor$coefficients)
-  }
-  else{
-    print("Predictor must go:",regressor$coefficients)
-  }
-}
-
-
