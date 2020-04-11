a86d4fcd10454f0e45423aaf9b824f3515994e85
Anastazija Verovic <anastazijaverovic@gmail.com>
2020-04-11 14:51:19 +0200

    Data visualisation of Training and Test dataset after using K-NN algorithm

--- Part 3 - Classification/Section 15 - K-Nearest Neighbors (K-NN)/classification_template.R
+++ Part 3 - Classification/Section 15 - K-Nearest Neighbors (K-NN)/classification_template.R
@@ -28,33 +28,51 @@ y_pred = predict(classifier, newdata = test_set[-3])
 # Making the Confusion Matrix
 cm = table(test_set[, 3], y_pred)
 
-# Visualising the Training set results
-library(ElemStatLearn)
+# Visualising Training set results
+
 set = training_set
 X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
 X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
-grid_set = expand.grid(X1, X2)
-colnames(grid_set) = c('Age', 'EstimatedSalary')
-y_grid = predict(classifier, newdata = grid_set)
-plot(set[, -3],
-     main = 'Classifier (Training set)',
-     xlab = 'Age', ylab = 'Estimated Salary',
-     xlim = range(X1), ylim = range(X2))
-contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
+
+grid_set = expand.grid('Age' = X1,
+                       'EstimatedSalary' = X2)
+
+y_grid = knn(train = training_set[, -3],
+             test = grid_set,
+             cl = training_set[, 3],
+             k = 5)
+
+plot (set[, -3],
+      main = 'K-NN (Training set)',
+      xlib = 'Age',
+      ylib = 'Estimated Salary',
+      xlim = range(X1),
+      ylim = range(X2))
+contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2), add = TRUE))
 points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'springgreen3', 'tomato'))
 points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
 
-# Visualising the Test set results
-library(ElemStatLearn)
+# Visualising Test set results
+
 set = test_set
+
 X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
 X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
-grid_set = expand.grid(X1, X2)
-colnames(grid_set) = c('Age', 'EstimatedSalary')
-y_grid = predict(classifier, newdata = grid_set)
-plot(set[, -3], main = 'Classifier (Test set)',
-     xlab = 'Age', ylab = 'Estimated Salary',
-     xlim = range(X1), ylim = range(X2))
-contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
+
+grid_set = expand.grid('Age' = X1,
+                       'EstimatedSalary' = X2)
+
+y_grid = knn(train = training_set[, -3],
+             test = grid_set,
+             cl = training_set[, 3],
+             k = 5)
+
+plot (set[, -3],
+      main = 'K-NN (Test set)',
+      xlib = 'Age',
+      ylib = 'Estimated Salary',
+      xlim = range(X1),
+      ylim = range(X2))
+contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2), add = TRUE))
 points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'springgreen3', 'tomato'))
 points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
--- Part 3 - Classification/Section 15 - K-Nearest Neighbors (K-NN)/knn.R
+++ Part 3 - Classification/Section 15 - K-Nearest Neighbors (K-NN)/knn.R
@@ -1,1 +1,80 @@
 # K-Nearest Neighbors (K-NN)
+
+# Importing the dataset
+dataset = read.csv('Social_Network_Ads.csv')
+dataset = dataset[3:5]
+
+# Encoding the target feature as factor
+dataset$Purchased = factor(dataset$Purchased, levels = c(0, 1))
+
+# Splitting the dataset into the Training set and Test set
+# install.packages('caTools')
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Purchased, SplitRatio = 0.75)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+# Feature Scaling
+training_set[-3] = scale(training_set[-3])
+test_set[-3] = scale(test_set[-3])
+
+# Fitting K-NN to the Training set and predicting the Test set results
+#install.packages('class')
+library(class)
+
+y_pred = knn(train = training_set[, -3],
+             test = test_set[, -3],
+             cl = training_set[, 3],
+             k = 5)
+
+# Making the Confusion Matrix
+cm = table(test_set[, 3], y_pred)
+
+set = training_set
+X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
+X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
+
+grid_set = expand.grid('Age' = X1,
+            'EstimatedSalary' = X2)
+
+y_grid = knn(train = training_set[, -3],
+                    test = grid_set,
+                    cl = training_set[, 3],
+                    k = 5)
+
+plot (set[, -3],
+      main = 'K-NN (Training set)',
+      xlib = 'Age',
+      ylib = 'Estimated Salary',
+      xlim = range(X1),
+      ylim = range(X2))
+contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2), add = TRUE))
+points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'springgreen3', 'tomato'))
+points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
+
+# Visualising Test set results
+
+set = test_set
+
+X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
+X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
+
+grid_set = expand.grid('Age' = X1,
+                       'EstimatedSalary' = X2)
+
+y_grid = knn(train = training_set[, -3],
+             test = grid_set,
+             cl = training_set[, 3],
+             k = 5)
+
+plot (set[, -3],
+      main = 'K-NN (Test set)',
+      xlib = 'Age',
+      ylib = 'Estimated Salary',
+      xlim = range(X1),
+      ylim = range(X2))
+contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2), add = TRUE))
+points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'springgreen3', 'tomato'))
+points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
+
