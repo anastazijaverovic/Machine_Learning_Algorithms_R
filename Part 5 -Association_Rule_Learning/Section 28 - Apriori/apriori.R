75d966e5063b02e766af0f2009757624c7d8765a
Anastazija Verovic <anastazijaverovic@gmail.com>
2020-10-07 21:34:38 +0200

    Apriori - training on the dataset, setting the support and default confidence

--- Part 5 -Association_Rule_Learning/Section 28 - Apriori/apriori.R
+++ Part 5 -Association_Rule_Learning/Section 28 - Apriori/apriori.R
@@ -1,17 +1,22 @@
 # Apriori Algorithm
 
 # Data Preprocessing
-
 dataset = read.csv('Market_Basket_Optimisation.csv', header = FALSE)
 
 # Sparse Matrix
-
 install.packages('arules')
 library(arules)
 
-sMatrix = read.transactions('Market_Basket_Optimisation.csv', sep = ',', rm.duplicates = TRUE)
+sMatrix = read.transactions('Market_Basket_Optimisation.csv', 
+                            sep = ',', 
+                            rm.duplicates = TRUE)
 summary(sMatrix)
 
 # Frequency Plot
-
 itemFrequencyPlot(x = sMatrix, topN = 10)
+
+# Training Apriori on the dataset
+# support of a product purchased 3 times a day = 3*7/total number of transactions
+rules = apriori(data = sMatrix, 
+                parameter = list(support = 0.003, 
+                                 confidence = 0.8))
