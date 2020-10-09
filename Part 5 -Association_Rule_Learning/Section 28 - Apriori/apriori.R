a5d18fab2ca18573f1a910b969b077e60825e035
Anastazija Verovic <anastazijaverovic@gmail.com>
2020-10-09 14:38:27 +0200

    Apriori Recommendation System - visualised the model on products sold in a grocery store. Support set regarding the regularity of buying the produce minimal 3 or 4 times a day

--- Part 5 -Association_Rule_Learning/Section 28 - Apriori/apriori.R
+++ Part 5 -Association_Rule_Learning/Section 28 - Apriori/apriori.R
@@ -4,7 +4,7 @@
 dataset = read.csv('Market_Basket_Optimisation.csv', header = FALSE)
 
 # Sparse Matrix
-install.packages('arules')
+#install.packages('arules')
 library(arules)
 
 sMatrix = read.transactions('Market_Basket_Optimisation.csv', 
@@ -17,6 +17,16 @@ itemFrequencyPlot(x = sMatrix, topN = 10)
 
 # Training Apriori on the dataset
 # support of a product purchased 3 times a day = 3*7/total number of transactions
+# confidence = 0.8, 0.4 or 0.2
+# support = 0.003 or 0.004 (product purchased 4 times a day)
+
 rules = apriori(data = sMatrix, 
                 parameter = list(support = 0.003, 
-                                 confidence = 0.8))
+                                 confidence = 0.2))
+
+# Visualising the rules descending
+
+inspect(sort(rules,by = 'lift')[1:10])
+
+
+
