a8579390e57cc8289032a576572ed03a85a8eb67
Anastazija Verovic <anastazijaverovic@gmail.com>
2020-11-06 19:26:47 +0100

    Reinforcement Learning - Upper Confidence Bound algorithm made on marketing campaign of 10 different ads on social network

---
+++ Part 6 - Reinforcement Learning/Section 32 - Upper Confidence Bound (UCB)/UCB.R
@@ -0,0 +1,18 @@
+# Upper Confidence Bound
+
+# Import the dataset
+
+dataset = read.csv("Ads_CTR_Optimisation.csv")
+
+# find out which version of the ad will result in the most clicks
+
+
+# round - each time a user logs in the website
+# 1 - user clicked on the ad
+
+# in each round - algorithm decides which ad to show to the user
+# on the basis of rewards of the rounds up to that round
+
+# 
+
+
