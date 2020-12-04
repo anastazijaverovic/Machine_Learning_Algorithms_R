a6960ef75066793d00d4de216051ceb3e6301584
Anastazija Verovic <anastazijaverovic@gmail.com>
2020-12-04 19:21:57 +0100

    Cleaning the text - transforming all the words in the corpus to lower case

--- Part 7 - Natural Language Processing/NLP.R
+++ Part 7 - Natural Language Processing/NLP.R
@@ -9,3 +9,21 @@ dataset = read.delim('Restaurant_Reviews.tsv',
                      quote = '', #ignore quotes in the text
                      stringsAsFactors = FALSE #reviews shouldn't be identified as factors (categorical)
                      )
+
+# Cleaning the review texts
+
+# create the independent variables to predict the dependent variable
+# dependent variable = liked
+# independent variables = distinct words
+
+# distinct words = columns
+# count the number of times the word goes with which review
+
+install.packages("slam", type = "binary")
+
+install.packages('tm')
+library(tm)
+
+corpus = VCorpus(VectorSource(dataset$Review))  #we won't clean the reviews directly in the dataset
+
+
