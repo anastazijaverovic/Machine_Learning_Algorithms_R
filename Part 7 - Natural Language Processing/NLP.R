f77071cebf74e3d6487442bccf3b4bca59f95e13
Anastazija Verovic <anastazijaverovic@gmail.com>
2020-12-04 18:34:47 +0100

    Imported the .tsv dataset

--- Part 7 - Natural Language Processing/NLP.R
+++ Part 7 - Natural Language Processing/NLP.R
@@ -2,3 +2,10 @@
 
 # Importing the dataset
 
+# get a dataset with columns separated by a tab
+# a sentence can contain a comma
+
+dataset = read.delim('Restaurant_Reviews.tsv',
+                     quote = '', #ignore quotes in the text
+                     stringsAsFactors = FALSE #reviews shouldn't be identified as factors (categorical)
+                     )
