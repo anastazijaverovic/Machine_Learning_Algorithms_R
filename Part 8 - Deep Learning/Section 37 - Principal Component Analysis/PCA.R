19150feeb4acfbd3064ee75af770827b9d321657
Anastazija Verovic <anastazijaverovic@gmail.com>
2021-01-03 21:31:48 +0100

    Used Support Vector Machine model to predict the Wine dataset's client segmentation, after the PCA dimensionality reduction

--- Part 8 - Deep Learning/Section 37 - Principal Component Analysis/PCA.R
+++ Part 8 - Deep Learning/Section 37 - Principal Component Analysis/PCA.R
@@ -37,49 +37,60 @@ training_set = training_set[c(2,3,1)]
 test_set = predict(pca, test_set)
 test_set = test_set[c(2,3,1)]
 
-# Fitting Logistic Regression to the Training set
+# Fitting SVM to Training set
 
-classifier = glm(formula = Purchased ~ .,
-                 family = binomial,
-                 data = training_set)
+classifier = svm(formula = Customer_Segment ~ .,
+                 data = training_set,
+                 type = 'C-classification',
+                 kernel = 'linear')
 
 # Predicting the Test set results
-
-# predicted probabilities vector
-prob_pred = predict(classifier, type = 'response', newdata = test_set[-3])
-
-# predicted values vector
-y_pred = ifelse(prob_pred > 0.5, 1, 0)
+y_pred = predict(classifier, newdata = test_set[-3])
 
 # Making the Confusion Matrix
-
 cm = table(test_set[, 3], y_pred)
 
 # Visualising Training set results
 
 set = training_set
+X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
+X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
 
-expand.grid('Age' = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01),
-            'EstimatedSalary' = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01))%>%
-  mutate(prob_set=predict(classifier, type = 'response', newdata = .),
-         y_grid = ifelse(prob_set > 0.5, 1, 0))%>%
-  
-  ggplot()+
-  geom_point(aes(x=Age, y=EstimatedSalary, color=ifelse(y_grid == 1,y_grid-0.2, -y_grid-0.2)))+
-  geom_point(data=training_set, aes(x=Age, y=EstimatedSalary,colour=as.numeric(Purchased)+0.5))
+grid_set = expand.grid('PC1' = X1,
+                       'PC2' = X2)
 
+y_grid = predict(classifier, newdata = grid_set)
 
-# Visualising Test set results
+plot (set[, -3],
+      main = 'SVM (Training set)',
+      xlib = 'PC1',
+      ylib = 'PC2',
+      xlim = range(X1),
+      ylim = range(X2))
+contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2), add = TRUE))
+points(grid_set, pch = '.', col = ifelse(y_grid == 2, 'deepskyblue', ifelse(y_grid == 1, 'springgreen3', 'tomato')))
+points(set, pch = 21, bg = ifelse(set[, 3] == 2, 'deepskyblue', ifelse(set[, 3] == 1, 'green4', 'red3')))
+
+# Visualising Test set results (prediction)
 
 set = test_set
 
-expand.grid('Age' = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01),#taking the range of the training set observation points for Age and Salary
-            'EstimatedSalary' = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01))%>%
-  mutate(prob_set=predict(classifier, type = 'response', newdata = .),#predicting the result of the graph using the classifier
-         y_grid = ifelse(prob_set > 0.5, 1, 0))%>%#transforming the predictions
-  
-  ggplot()+
-  geom_point(aes(x=Age, y=EstimatedSalary, color=ifelse(y_grid == 1,y_grid-0.2, -y_grid-0.2)))+
-  geom_point(data=test_set, aes(x=Age, y=EstimatedSalary,colour=as.numeric(Purchased)+0.5))
+X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
+X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
+
+grid_set = expand.grid('PC1' = X1,
+                       'PC2' = X2)
+
+y_grid = predict(classifier, newdata = grid_set)
+
+plot (set[, -3],
+      main = 'SVM (Test set)',
+      xlib = 'PC1',
+      ylib = 'PC2',
+      xlim = range(X1),
+      ylim = range(X2))
+contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2), add = TRUE))
+points(grid_set, pch = '.', col = ifelse(y_grid == 2, 'deepskyblue', ifelse(y_grid == 1, 'springgreen3', 'tomato')))
+points(set, pch = 21, bg = ifelse(set[, 3] == 2, 'deepskyblue', ifelse(set[, 3] == 1, 'green4', 'red3')))
 
 
